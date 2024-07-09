cc159bfa5c1b6beaad1ed118d343a74de8b21b57
KitEA <kit.earf@gmail.com>
2024-07-09 16:35:48 +0400

    redo test logic

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -5,93 +5,123 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class GameTest {
 
-    private Game g;
+    private Game game;
 
     @BeforeEach
     void setUp() {
-        g = new Game();
+        game = new Game();
     }
 
-    public void rollMany(int n, int pins) {
+    @Test
+    void shouldCalculateScoreForStrike() {
+        // when
+        rollStrike();
+
+        // then
+        assertThat(game.score()).isEqualTo(10);
+    }
+
+    @Test
+    void shouldCalculateScoreForStrikeAndRoll() {
+        // given
+        rollStrike();
+
+        // when
+        game.roll(4);
+
+        // then
+        assertThat(game.score()).isEqualTo(14);
+    }
+
+    @Test
+    void shouldCalculateScoreForTwoStrikes() {
+        // given
+        rollStrike();
+
+        // when
+        rollStrike();
+
+        // then
+        assertThat(game.score()).isEqualTo(20);
+    }
+
+    @Test
+    void shouldCalculateScoreForRollAfterTwoStrikes() {
+        // given
+        rollStrike();
+        rollStrike();
+
+        // when
+        game.roll(5);
+
+        // then
+        assertThat(game.score()).isEqualTo(40);
+    }
+
+    @Test
+    void shouldCalculateScoreForSpare() {
+        // when
+        rollSpare();
+
+        // then
+        assertThat(game.score()).isEqualTo(10);
+    }
+
+    @Test
+    void shouldCalculateScoreForRollAfterSpare() {
+        // given
+        rollSpare();
+
+        // when
+        game.roll(4);
+
+        // then
+        assertThat(game.score()).isEqualTo(18);
+    }
+
+    @Test
+    void shouldCalculateScoreForTwoRollsAfterSpare() {
+        // given
+        rollSpare();
+
+        // when
+        game.roll(4);
+        game.roll(4);
+
+        // then
+        assertThat(game.score()).isEqualTo(22);
+    }
+
+    @Test
+    void shouldReturnZeroScoreForAllZeroRolls() {
+        rollMany(20, 0);
+        assertThat(game.score()).isEqualTo(0);
+    }
+
+    @Test
+    void shouldCalculateScoreForAllOnesRolls() {
+        rollMany(20, 1);
+        assertThat(game.score()).isEqualTo(20);
+    }
+
+    @Test
+    void shouldCalculateScoreForPerfectGame() {
+        rollMany(12, 10);
+        assertThat(game.score()).isEqualTo(300);
+    }
+
+    private void rollMany(int n, int pins) {
         for (int i = 0; i < n; i++) {
-            g.roll(pins);
+            game.roll(pins);
         }
     }
 
-    @Test
-    void testGutterGame() {
-        rollMany(20, 0);
-        assertThat(g.score()).isEqualTo(0);
+    private void rollStrike() {
+        game.roll(10);
     }
 
-    @Test
-    void allOne() {
-        rollMany(20, 1);
-        assertThat(g.score()).isEqualTo(20);
-    }
-
-    @Test
-    void testSpare() {
-        g.roll(5);
-        g.roll(5);
-        g.roll(3);
-        rollMany(17, 0);
-        assertThat(g.score()).isEqualTo(16);
-    }
-
-    @Test
-    void testStrike() {
-        g.roll(10);
-        g.roll(3);
-        g.roll(6);
-        rollMany(16, 0);
-        assertThat(g.score()).isEqualTo(28);
-    }
-
-    @Test
-    void testFull() {
-        g.roll(1);
-        g.roll(4);
-        g.roll(4);
-        g.roll(5);
-        g.roll(6);
-        g.roll(4);
-        g.roll(5);
-        g.roll(5);
-        g.roll(10);
-        g.roll(0);
-        g.roll(1);
-        rollMany(8, 0);
-        assertThat(g.score()).isEqualTo(61);
-    }
-
-    @Test
-    void testTenthFrame() {
-        g.roll(1);
-        g.roll(4);
-        g.roll(4);
-        g.roll(5);
-        g.roll(6);
-        g.roll(4);
-        g.roll(5);
-        g.roll(5);
-        g.roll(10);
-        g.roll(0);
-        g.roll(1);
-        g.roll(7);
-        g.roll(3);
-        g.roll(6);
-        g.roll(4);
-        g.roll(10);
-        g.roll(2);
-        g.roll(8);
-        g.roll(6);
-        assertThat(g.score()).isEqualTo(133);
-    }
-
-    @Test
-    void testPerfectGame() {
-        rollMany(12, 10);
-        assertThat(g.score()).isEqualTo(300);
+    private void rollSpare() {
+        game.roll(5);
+        game.roll(5);
     }
 }
